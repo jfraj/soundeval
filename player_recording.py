45fc3221f7d9f1bf8558ab3a780d7b6232e5f391
jfraj <rajotte.jeanfrancois@gmail.com>
2015-06-25 10:03:07 -0400

    adding visualization option to recorder

--- player_recording.py
+++ player_recording.py
@@ -1,18 +1,23 @@
 import os
 import time
-import recorder
 
+#  This project
+import recorder
+import check_signal
 
 def record_playing(**kwargs):
     """Record audio signal and save it."""
     save_dir = kwargs.get('save_dir', 'test/')
-    basename = raw_input('Type a name for the file?')
+    show_audio = kwargs.get('show_audio', False)
+    basename = raw_input('Type a name for the file? ')
     save_name = '{}_{}.wav'.format(basename, time.strftime("%Y%m%d"))
     save_name = os.path.join(save_dir, save_name)
     raw_input('Press enter when ready to start...')
     rec = recorder.AudioRecorder(max_length=60)
     rec.start_record(savename=save_name)
+    if show_audio:
+        check_signal.audio_report(save_name)
 
 
 if __name__ == "__main__":
-    record_playing()
+    record_playing(show_audio=True)
